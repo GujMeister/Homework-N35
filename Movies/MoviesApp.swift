94a7d68dba2cd444bc002a5db840247f9bab009d
GujMeister <90295019+GujMeister@users.noreply.github.com>
2024-06-04 23:24:12 +0400

    Initial Commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Luka Gujejiani on 04.06.24.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
